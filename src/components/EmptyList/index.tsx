17b23668a10a995704299a48bae26902c0b1efad
Roger Oliveira <rogerpolvr@gmail.com>
2023-06-16 20:02:03 -0300

    refactor: add hr above EmptyList component

--- src/components/EmptyList/index.tsx
+++ src/components/EmptyList/index.tsx
@@ -3,10 +3,15 @@ import Icon from "react-native-vector-icons/Feather";
 
 export function EmptyList() {
   return (
-    <View className="flex-column items-center justify-center" >
+    <View className="flex-column items-center justify-center">
+      <View className="border my-[4%] mb-[20%] border-black w-[100%]" />
       <Icon name="inbox" size={100} color="black" />
-      <Text className="font-bold text-xl">You don't have any tasks lined up yet</Text>
-      <Text className="font-light text-lg">Create tasks and organize your to-do items</Text>
+      <Text className="font-bold text-xl">
+        You don't have any tasks lined up yet
+      </Text>
+      <Text className="font-light text-lg">
+        Create tasks and organize your to-do items
+      </Text>
     </View>
   );
 }
